a2952fd38d87f990cdaa25759f0d767e825d2c78
Patrique Ouimet <patrique.ouimet@gmail.com>
2023-04-16 07:51:49 -0400

    chore: revised chapter 13 exercises

--- chapter_13/exercise_1.php
+++ chapter_13/exercise_1.php
@@ -4,8 +4,9 @@ function highestProduct(array $numbers): int {
 	sort($numbers);
 	$last_number = array_pop($numbers) ?? 1;
 	$second_last_number = array_pop($numbers) ?? 1;
+	$third_last_number = array_pop($numbers) ?? 1;
 
-	return $second_last_number * $last_number; 
+	return $third_last_number * $second_last_number * $last_number; 
 }
 
 var_dump(highestProduct([5,2,3,1,4]));
