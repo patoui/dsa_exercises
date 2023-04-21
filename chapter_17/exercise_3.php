755d17bd8fdcfd5f13b075821386187b3747cad9
Patrique Ouimet <patrique.ouimet@gmail.com>
2023-04-21 08:25:46 -0400

    chore: completed chapter 17 exercise 3

---
+++ chapter_17/exercise_3.php
@@ -0,0 +1,68 @@
+<?php
+
+class TrieNode
+{
+	public function __construct(public array $children = []) {}
+}
+
+class Trie
+{
+	public function __construct(public TrieNode $root = new TrieNode()) {}
+
+	public function printAll(): void
+	{
+		foreach ($this->collectAllWords() as $word) {
+			echo $word . PHP_EOL;
+		}
+	}
+
+	public function collectAllWords(
+		TrieNode $node = null,
+		string $word = '',
+		array &$words = []
+	): array {
+		$currentNode = $node ?? $this->root;
+
+		foreach ($currentNode->children as $key => $childNode) {
+			if ($key === '*') {
+				$words[] = $word . $key;
+			} else {
+				$this->collectAllWords(
+					$childNode,
+					$word . $key,
+					$words
+				);
+			}
+		}
+
+		return $words;
+	}
+
+	public function insert(string $word): void
+	{
+		$currentNode = $this->root;
+
+		for ($i = 0; $i < strlen($word); $i++) {
+			$char = $word[$i];
+
+			if (isset($currentNode->children[$char])) {
+				$currentNode = $currentNode->children[$char];
+			} else {
+				$newNode = new TrieNode();
+				$currentNode->children[$char] = $newNode;
+				$currentNode = $newNode;
+			}
+		}
+
+		$currentNode->children['*'] = null;
+	}
+}
+
+$trie = new Trie();
+$trie->insert('ace');
+$trie->insert('act');
+$trie->insert('we');
+$trie->insert('well');
+$trie->insert('went');
+$trie->printAll();
+
