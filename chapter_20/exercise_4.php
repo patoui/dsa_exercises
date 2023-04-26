a86e8b9ef243df8be64e982caa5e09e52f1728b1
Patrique Ouimet <patrique.ouimet@gmail.com>
2023-04-26 09:24:41 -0400

    chore: revised chapter 20 exercise 4

--- chapter_20/exercise_4.php
+++ chapter_20/exercise_4.php
@@ -8,33 +8,24 @@ function maxProduct(array $nums): int {
 
         foreach ($nums as $num) {
                 if ($num > $max_1) {
-                        $temp = $max_1;
+                        $max_2 = $max_1;
                         $max_1 = $num;
-                        $max_2 = $temp;
                 } elseif ($num > $max_2) {
                         $max_2 = $num;
                 }
 
                 if ($num < 0) {
                         if ($num < $min_1) {
-                                $temp = $min_1;
+                                $min_2 = $max_1;
                                 $min_1 = $num;
-                                $min_2 = $temp;
                         } elseif ($num < $min_2) {
                                 $min_2 = $num;
                         }
                 }
         }
 
-        $min_prod = PHP_INT_MIN;
-        if ($min_1 !== PHP_INT_MAX && $min_2 !== PHP_INT_MAX) {
-                $min_prod = $min_1 * $min_2;
-        }
-
-        $max_prod = PHP_INT_MIN;
-        if ($max_1 !== PHP_INT_MIN && $max_2 !== PHP_INT_MIN) {
-                $max_prod = $max_1 * $max_2;
-        }
+        $min_prod = $min_1 * $min_2;
+        $max_prod = $max_1 * $max_2;
 
         return $min_prod > $max_prod ? $min_prod : $max_prod;
 }
