427f4fbb83acda75bf731d458160af01b9be9bc7
Patrique Ouimet <patrique.ouimet@gmail.com>
2023-04-26 09:18:59 -0400

    chore: revised chapter 20 exercise 2

--- chapter_20/exercise_2.php
+++ chapter_20/exercise_2.php
@@ -11,8 +11,28 @@ function missingNumber(array $integers): int
         return current($num_map);
 }
 
+function missingNumberAlt(array $integers): int
+{
+        $full_sum = 0;
+        foreach (range(1, count($integers)) as $i) {
+                $full_sum += $i;
+        }
+
+        $current_sum = 0;
+        foreach ($integers as $num) {
+                $current_sum += $num;
+        }
+
+        return $full_sum - $current_sum;
+}
+
 echo missingNumber([2,3,0,6,1,5]) . PHP_EOL;
 echo missingNumber([8,2,3,9,4,7,5,0,6]) . PHP_EOL;
 echo missingNumber([4,3,2,1]) . PHP_EOL;
 echo missingNumber([0,1,2,4]) . PHP_EOL;
+
+echo missingNumberAlt([2,3,0,6,1,5]) . PHP_EOL;
+echo missingNumberAlt([8,2,3,9,4,7,5,0,6]) . PHP_EOL;
+echo missingNumberAlt([4,3,2,1]) . PHP_EOL;
+echo missingNumberAlt([0,1,2,4]) . PHP_EOL;
 
